1ac1a792e159a359fbe7ab0a560e14d629a68d82
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-10 12:55:32 -0700

    added knex file migration

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,18 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/db3'
-    }
+      filename: './database/user.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   }
-
 };
