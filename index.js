c262c8a1eaa4240db6fee0bb5367a93a064c4673
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-10 14:01:22 -0700

    password dcrypted

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const express = require('express');
-const helmet = require('helmet');
-
-
-
-
-const server = express();
-server.use(helmet());
-server.use(express.json());
-
+const server = require('./api/server.js');
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
