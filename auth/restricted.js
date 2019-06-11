78dc1a36ced8ff88da95f2614b93e886fd0bcabd
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-06-11 11:18:24 -0700

    changed restricted

--- auth/restricted.js
+++ auth/restricted.js
@@ -4,22 +4,27 @@ const Users = require('../users/users-model');
 
 // middleware
 module.exports = function restricted(req, res, next) {
-  const { username, password } = req.headers;
+  // const { username, password } = req.headers;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
+  // if (username && password) {
+  //   Users.findBy({ username })
+  //     .first()
+  //     .then(user => {
+  //       if (user && bcrypt.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ message: 'Invalid Credentials' });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       res.status(500).json(error);
+  //     });
+  // } else {
+  //   res.status(400).json({ message: 'Please provide credentials' });
+  // }
+  if(req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({ message: 'Please provide credentials' });
+    res.status(401).json({message: 'You shall not pass!' })
   }
 };
